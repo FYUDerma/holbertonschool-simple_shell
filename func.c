461e38c019a555317d4c3a020f420e7126f260f9
yanis59100 <meftouh.yanis@hotmail.com>
2024-08-21 15:26:22 +0200

    remove trailing whitespace from string

--- func.c
+++ func.c
@@ -14,3 +14,30 @@ int _isspace(int c)
 	}
 	return (0);
 }
+
+/**
+ * trim - removes leading and trailing whitespace from string
+ * @str: string to trim
+ * Return: trimmed string
+ */
+char *trim(char *str)
+{
+	char *end = str + strlen(str) - 1;
+
+	while (_isspace(*str))
+	{
+		str++;
+	}
+
+	if (*str == 0)
+		return (str);
+
+	while (end > str && _isspace(*end))
+	{
+		end--;
+	}
+
+	end[1] = '\0';
+
+	return (str);
+}
